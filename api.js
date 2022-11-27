1d77aa8579c90b6c596822b1fe54be50d83780c6
Vila Juan Cruz <yunagosh7@gmail.com>
2022-11-26 22:35:18 -0300

    Añadi el cors

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const api = require('./api.json')
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const app = express();
 
-const app = express()
+app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.send(api)
+    res.send(api);
 })
 
 console.log(PORT)
